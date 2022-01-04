30bb3f807b769717a2b15052490b6f67a853d1f8
MajorChe <charithnanduri03@gmail.com>
2022-01-03 22:36:04 -0500

    add and include module

--- 06_inhereitence.rb
+++ 06_inhereitence.rb
@@ -1,13 +1,26 @@
+module Ev
+  def hybrid
+    @year < 2019 ? false : true
+  end
+end
+
 class Vehicle
   attr_accessor :color, :model
   attr_reader :year
+
+  @@number_of_vehicles = 0
+
   def initialize(year,model,color)
     @speed = 0
     @year = year
     @color = color
     @model = model
+    @@number_of_vehicles += 1
   end
 
+  def self.number_of_vehicles
+    puts "Number of vehicles created: #{@@number_of_vehicles}"
+  end
   def speed
     puts "current speed is #{@speed} kmph"
   end
@@ -40,21 +53,24 @@ end
 
 class MyCar < Vehicle
   NUMBER_OF_DOORS = 4
+  include Ev
   def to_s
-    puts "My car is #{@model}"
+    puts "My car is #{@year} #{@model}"
   end
 end
 
 class MyTruck < Vehicle
   NUMBER_OF_DOORS = 2
   def to_s
-    puts "My Truck is #{@model}"
+    puts "My Truck is #{@year} #{@model}"
   end
 end
 
 
-car1 = MyCar.new(2021,"Range Rover Sport","White")
+car1 = MyCar.new(2018,"Range Rover Sport","White")
 truck1 = MyTruck.new(2020,"Ford F150","Black")
 
 puts car1
+puts "My car is hybrid capable: #{car1.hybrid}"
 puts truck1
+puts Vehicle.number_of_vehicles
